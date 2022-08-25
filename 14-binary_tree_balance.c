743e010cc53231cd8032683849bcd04312dfedb3
MwangiGregory <gmwaburi@gmail.com>
2022-08-25 14:20:56 +0000

    Redifine binary_tree_height function

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -34,16 +34,9 @@ size_t binary_tree_height(const binary_tree_t *tree)
 	{
 		size_t l = 0, r = 0;
 
-		if (tree->left)
-			l = 1 + binary_tree_height(tree->left);
-		else
-			l = 1;
-		if (tree->right)
-			r = 1 + binary_tree_height(tree->right);
-		else
-			r = 1;
+		l = tree->left ? 1 + binary_tree_height(tree->left) : 1;
+		r = tree->right ? 1 + binary_tree_height(tree->right) : 1;
 		return ((l > r) ? l : r);
 	}
 	return (0);
 }
-
